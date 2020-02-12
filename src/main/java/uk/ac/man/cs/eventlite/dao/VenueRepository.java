f865e1a49dccc697c9f114022d4dae341ea3e114
Henry Rubiano-Poveda <henry.rubianopoveda@student.manchester.ac.uk>
2020-02-12 12:27:47 +0000

    Create Venue Repository, Modify VenueService and VenueServiceImpl
    - Modify VenueService and VenueServiceImpl by now calling to the new
    created VenueRepository class, which extends CRUDRepository that will
    implement the necessary methods for us.

--- src/main/java/uk/ac/man/cs/eventlite/dao/VenueRepository.java
+++ src/main/java/uk/ac/man/cs/eventlite/dao/VenueRepository.java
@@ -4,9 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import uk.ac.man.cs.eventlite.entities.Venue;
 
-public interface VenueRepository  extends CrudRepository<Venue, Long>{
-	public Iterable<Venue> findAll();
-	public long count();
-	public <S extends Venue> S save(S entity);
-
+public interface VenueRepository extends CrudRepository<Venue, Long>{
+	
 }
