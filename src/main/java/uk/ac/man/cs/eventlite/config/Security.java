d3269f3c8aa4d706d80ebf77c3d3c55c3185ba69
Joana Cruz <joana.cruz@student.manchester.ac.uk>
2020-02-25 11:07:08 +0000

    Tests Pass, Role base implementation rolled back but legacy was left

--- src/main/java/uk/ac/man/cs/eventlite/config/Security.java
+++ src/main/java/uk/ac/man/cs/eventlite/config/Security.java
@@ -24,7 +24,9 @@ public class Security extends WebSecurityConfigurerAdapter {
 	// List the mappings/methods for which no authorisation is required.
 	// By default we allow all GETs and full access to the H2 console.
 	private static final RequestMatcher[] NO_AUTH = { new AntPathRequestMatcher("/webjars/**", "GET"),
-			 new AntPathRequestMatcher("/h2-console/**") };
+			 new AntPathRequestMatcher("/h2-console/**"),new AntPathRequestMatcher("/**", "GET")};
+	// comment new AntPathRequestMatcher("/**", "GET") for ole base to work
+	// for now not applied since tests would need changing
 	
 	private static final RequestMatcher[] A = { new AntPathRequestMatcher("/events/**", "GET") };
 	//new AntPathRequestMatcher("/**", "GET")
