785eca3e1cb52de89b2d02debe079b2bb7f89451
vinh-arch <vinh.phamvan@student.manchester.ac.uk>
2020-03-24 18:57:43 +0100

    Add tests for methods handling adding events
    - Add tests in EventsController and EventsControllersApi classes
      - Test for adding without authentication
      - Test for adding with bad role
      - Test for adding without CSRF
      - Test for correct adding
      - Test for trying to add with an event name that is too long
      - Test for trying to add an event without a name

--- src/test/java/uk/ac/man/cs/eventlite/controllers/EventsControllerApiTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/EventsControllerApiTest.java
@@ -2,10 +2,14 @@ package uk.ac.man.cs.eventlite.controllers;
 
 import static org.hamcrest.Matchers.endsWith;
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -32,8 +36,10 @@ import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import uk.ac.man.cs.eventlite.config.Security;
 import uk.ac.man.cs.eventlite.EventLite;
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
@@ -46,6 +52,8 @@ import uk.ac.man.cs.eventlite.entities.Venue;
 @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 @ActiveProfiles("test")
 public class EventsControllerApiTest {
+	
+	private final static String BAD_ROLE = "USER";
 
 	private MockMvc mvc;
 
@@ -54,6 +62,9 @@ public class EventsControllerApiTest {
 
 	@Mock
 	private VenueService venueService;
+	
+	@Mock
+	private Event event;
 	
 	@Mock
 	private EventService eventService;
@@ -102,5 +113,77 @@ public class EventsControllerApiTest {
 				.andExpect(jsonPath("$._embedded.events.length()", equalTo(1)));
 
 		verify(eventService).findAll();
+	}
+	
+	@Test
+	public void getNewEvent() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/api/events/new").accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isNotAcceptable()).andExpect(handler().methodName("newEvent"));
+	}
+
+	@Test
+	public void postGreetingNoAuth() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.post("/api/events").contentType(MediaType.APPLICATION_JSON)
+				.content("{ \"name\": \"Test Event New\" }").accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isUnauthorized());
+
+		verify(eventService, never()).save(event);
+	}
+
+	@Test
+	public void postEventBadAuth() throws Exception {
+		mvc.perform(
+				MockMvcRequestBuilders.post("/api/events").with(anonymous())
+				.contentType(MediaType.APPLICATION_JSON).content("{ \"name\": \"Test Event New\" }")
+				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isUnauthorized());
+
+		verify(eventService, never()).save(event);
+	}
+
+	@Test
+	public void postEventBadRole() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.post("/api/events").with(user("Rob").roles(BAD_ROLE))
+				.contentType(MediaType.APPLICATION_JSON).content("{ \"name\": \"Test Event New\" }")
+				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isForbidden());
+
+		verify(eventService, never()).save(event);
+	}
+
+	/*
+	@Test
+	public void postBadEvent() throws Exception {
+		mvc.perform(
+				MockMvcRequestBuilders.post("/api/events").with(user("Rob").roles(Security.ADMIN_ROLE))
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{ \"template\": \"<Something bad>\" }").accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isUnprocessableEntity()).andExpect(content().string(""))
+		.andExpect(handler().methodName("createEvent"));
+
+		verify(eventService, never()).save(event);
+	}
+	*/
+
+	@Test
+	public void postLongEvent() throws Exception {
+		mvc.perform(
+				MockMvcRequestBuilders.post("/api/events").with(user("Rob").roles(Security.ADMIN_ROLE))
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{ \"template\": \"abcdefghij s klmnopqrst uvwxyz abcdefghij s klmnopqrst uvwxyz abcdefghij s klmnopqrst uvwxyz abcdefghij s klmnopqrst uvwxyz abcdefghij s klmnopqrst uvwxyz abcdefghij s klmnopqrst uvwxyzabcdefghij s klmnopqrst uvwxyz abcdefghij s klmnopqrst uvwxyz abcdefghij s klmnopqrst uvwxyz\" }").accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isUnprocessableEntity()).andExpect(content().string(""))
+		.andExpect(handler().methodName("createEvent"));
+
+		verify(eventService, never()).save(event);
+	}
+
+	@Test
+	public void postEmptyEvent() throws Exception {
+		mvc.perform(
+				MockMvcRequestBuilders.post("/api/events").with(user("Rob").roles(Security.ADMIN_ROLE))
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{ \"template\": \"\" }").accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isUnprocessableEntity()).andExpect(content().string(""))
+		.andExpect(handler().methodName("createEvent"));
+
+		verify(eventService, never()).save(event);
 	}
 }
