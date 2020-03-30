bd02b1ad77df50c9dc0cf74794af43ccf0c1963d
vinh-arch <vinh.phamvan@student.manchester.ac.uk>
2020-03-30 12:04:15 +0200

    Implement adding a new venue
    - Change Venue class to include validations
    - Add road name and postcode to Venue class
    - Add button to index for adding new venue
    - Add new page for adding a new venue
    - Add VenuesController and VenuesControllerApi classes for handling
    adding a venue

--- src/main/java/uk/ac/man/cs/eventlite/entities/Venue.java
+++ src/main/java/uk/ac/man/cs/eventlite/entities/Venue.java
@@ -3,6 +3,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "venues")
@@ -12,8 +15,19 @@ public class Venue {
 	@GeneratedValue
 	private long id;
 
+	@NotEmpty(message = "Name required")
+	@Size(max = 256, message = "The venue name must have 256 characters or less")
 	private String name;
+	
+	@NotEmpty(message = "Road name required")
+	@Size(max = 300, message = "The road name must have 300 characters or less")
+	private String roadName;
+	
+	@NotEmpty(message = "Postcode required")
+	@Size(max = 256, message = "The postcode must have 256 characters or less")
+	private String postCode;
 
+	@Min(value = 0, message = "The venue capacity must be a positive integer")
 	private int capacity;
 
 	public Venue() {
@@ -33,6 +47,22 @@ public class Venue {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public String getRoadName() {
+		return roadName;
+	}
+
+	public void setRoadName(String roadName) {
+		this.roadName = roadName;
+	}
+	
+	public String getPostCode() {
+		return postCode;
+	}
+
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
 	}
 
 	public int getCapacity() {
