12d42aefb2607a122278074138bf50c1677596a6
Isabelle Hall <isabelle.hall@student.manchester.ac.uk>
2020-04-08 13:31:33 +0100

    Implemented venue update methods in venue Controller

--- src/main/java/uk/ac/man/cs/eventlite/controllers/VenuesController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/VenuesController.java
@@ -10,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -85,6 +86,35 @@ public class VenuesController {
 		
 
 		return "venues/venue_details";
+	}
+	
+	@GetMapping("/updateVenue/{id}")
+    public String getVenueToUpdate(Model model, @PathVariable("id") Long id, RedirectAttributes redirectAttrs) {
+	 
+	 	Venue venue = venueService.findOne(id);
+	 	
+	 	if(venue==null) {
+	 		redirectAttrs.addFlashAttribute("error_message", "venue not found");
+	 	}
+	 	
+		model.addAttribute("venue", venue);
+		model.addAttribute("venueList", venueService.findAll());
+		
+        return "venues/updateVenue";
+    }
+
+	@RequestMapping(value="/update/{id}", method= RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public String putEvent(@PathVariable("id") Long id, Venue venue) {
+		 
+		Venue newVenue = venueService.findOne(id);
+		newVenue.setName(venue.getName());
+		newVenue.setPostCode(venue.getPostCode());
+		newVenue.setRoadName(venue.getRoadName());
+		newVenue.setCapacity(venue.getCapacity());;
+		
+		venueService.save(newVenue);
+
+		return "redirect:/venues";
 	}
 	
 }
