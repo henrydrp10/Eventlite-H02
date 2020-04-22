97e8b986a963b4d1d6fc6c3beec20f1f6b1dcf75
c67172vi <vlad.iacob@student.manchester.ac.uk>
2020-04-22 12:57:45 +0100

    Fix the get method for HomeControllerApi
    - Now the get method is implemented and will no longer return 404. The
    json file contains a 'Home Page' label and the required 2 links + one
    for profile. In addition it also contains links for itself and the three
    upcoming events and vanues.

--- src/main/java/uk/ac/man/cs/eventlite/controllers/HomeControllerApi.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/HomeControllerApi.java
@@ -22,13 +22,28 @@ import uk.ac.man.cs.eventlite.entities.Event;
 import uk.ac.man.cs.eventlite.entities.Venue;
 
 @RestController
-@RequestMapping(value = "/api/", produces = { MediaType.APPLICATION_JSON_VALUE, MediaTypes.HAL_JSON_VALUE })
+@RequestMapping(value = "/api", produces = { MediaType.APPLICATION_JSON_VALUE, MediaTypes.HAL_JSON_VALUE })
 public class HomeControllerApi {
 
 	@Autowired
 	private EventService eventService;
 	
 	@RequestMapping(method = RequestMethod.GET)
+	public Resource<String> body()
+	{
+		Link selfLink = linkTo(HomeControllerApi.class).withSelfRel();
+		Link venueLink = linkTo(VenuesControllerApi.class).withRel("venues");
+		Link eventLink = linkTo(EventsControllerApi.class).withRel("events");
+		//update this if the profileController will be made in the future.
+		Link profileLink = new Link("http://localhost:8080/api/profile").withRel("profile");
+		
+		//put the links relative to the controller in the future.
+		Link threeUpcomingEventsLink = new Link("http://localhost:8080/api/getThreeUpcomingEvents").withRel("Three Upcoming Events");
+		Link threeMostUsedVenues = new Link("http://localhost:8080/api/getThreeMostUsedVenues").withRel("Three Most Used Venues");
+		return new Resource<String>("Home Page", venueLink, eventLink, profileLink, selfLink, threeUpcomingEventsLink, threeMostUsedVenues);
+	}
+	
+	@RequestMapping(value = "/getThreeUpcomingEvents", method = RequestMethod.GET)
 	public Resources<Resource<Event>> getThreeUpcomingEvents() {
 		return eventToResource(eventService.threeUpcomingEvents());
 	}
@@ -50,27 +65,27 @@ public class HomeControllerApi {
 		return new Resources<Resource<Event>>(resources, selfLink);
 	}
 	
-//	@RequestMapping(method = RequestMethod.GET)
-//	public Resources<Resource<Venue>> getThreeMostUsedVenues() {
-//
-//		return venueToResource(eventService.threeMostUsedVenues());
-//	}
-//
-//	private Resource<Venue> venueToResource(Venue venue) {
-//		Link selfLink = linkTo(VenuesControllerApi.class).slash(venue.getId()).withSelfRel();
-//
-//		return new Resource<Venue>(venue, selfLink);
-//	}
-//
-//	private Resources<Resource<Venue>> venueToResource(Iterable<Venue> venues) {
-//		Link selfLink = linkTo(methodOn(VenuesControllerApi.class).getAllVenues()).withSelfRel();
-//
-//		List<Resource<Venue>> resources = new ArrayList<Resource<Venue>>();
-//		for (Venue venue : venues) {
-//			resources.add(venueToResource(venue));
-//		}
-//
-//		return new Resources<Resource<Venue>>(resources, selfLink);
-//	}
+	@RequestMapping(value = "/getThreeMostUsedVenues", method = RequestMethod.GET)
+	public Resources<Resource<Venue>> getThreeMostUsedVenues() {
+
+		return venueToResource(eventService.threeMostUsedVenues());
+	}
+
+	private Resource<Venue> venueToResource(Venue venue) {
+		Link selfLink = linkTo(VenuesControllerApi.class).slash(venue.getId()).withSelfRel();
+
+		return new Resource<Venue>(venue, selfLink);
+	}
+
+	private Resources<Resource<Venue>> venueToResource(Iterable<Venue> venues) {
+		Link selfLink = linkTo(methodOn(VenuesControllerApi.class).getAllVenues()).withSelfRel();
+
+		List<Resource<Venue>> resources = new ArrayList<Resource<Venue>>();
+		for (Venue venue : venues) {
+			resources.add(venueToResource(venue));
+		}
+
+		return new Resources<Resource<Venue>>(resources, selfLink);
+	}
 	
 }
