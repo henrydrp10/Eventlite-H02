791211374c78397106ce778ba06999478ed12024
w27974jc <joana.cruz@student.manchester.ac.uk>
2020-04-28 03:06:41 +0100

    Write tests for update venue
    Within this process a bug was found since if some invalid parameters are
    given to the update Venue form, it would not show any errors. For now
    the quick fix available is doing a manual check within an if statement
    in the respective controller. In future commits it is expectable that
    this check is performed within the html form with help of thyme leaf
    (idea: use same strategy as the one for checking the add venue form
    which is fully working).

--- src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerTest.java
@@ -3,13 +3,15 @@ package uk.ac.man.cs.eventlite.controllers;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.endsWith;
 import static org.hamcrest.Matchers.equalTo;
-
+import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.flash;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
@@ -17,7 +19,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Collections;
 
 import javax.servlet.Filter;
 
@@ -81,6 +86,28 @@ public class VenuesControllerTest {
 		mvc = MockMvcBuilders.standaloneSetup(venuesController).apply(springSecurity(springSecurityFilterChain))
 				.build();
 	}
+	
+	@Test
+	public void getIndexWhenNoVenues() throws Exception {
+		
+		when(venueService.findAll()).thenReturn(Collections.<Venue> emptyList());
+
+		mvc.perform(get("/venues").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+				.andExpect(view().name("venues/index")).andExpect(handler().methodName("getAllVenues"));
+
+		verify(venueService).findAll();
+	}
+
+	@Test
+	public void getIndexWithVenues() throws Exception {
+		
+		when(venueService.findAll()).thenReturn(Collections.<Venue> singletonList(venue));
+
+		mvc.perform(get("/venues").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+				.andExpect(view().name("venues/index")).andExpect(handler().methodName("getAllVenues"));
+
+		verify(venueService).findAll();
+	}
 
 	public void getNewVenueNoAuth() throws Exception {		
 		mvc.perform(MockMvcRequestBuilders.post("/venues")
@@ -95,6 +122,34 @@ public class VenuesControllerTest {
 				.accept(MediaType.TEXT_HTML))
 		.andExpect(status().isOk()).andExpect(view().name("venues/new"))
 		.andExpect(handler().methodName("newVenue"));
+	}
+	
+	public void getUpdateVenueViewNoAuth() throws Exception {
+		Venue v = new Venue();
+		v.setName("Venue");
+		v.setCapacity(1000);
+		venueService.save(v);
+		long id = v.getId();
+		when(venueService.findOne(id)).thenReturn(v);
+
+		mvc.perform(MockMvcRequestBuilders.post("/venues")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("name", "abcdefghij").accept(MediaType.TEXT_HTML).with(csrf()))
+		.andExpect(status().isFound()).andExpect(header().string("Location", endsWith("/sign-in")));
+	}
+
+	@Test
+	public void getUpdateVenueView() throws Exception {
+		Venue v = new Venue();
+		v.setName("Venue");
+		v.setCapacity(1000);
+		venueService.save(v);
+		long id = v.getId();
+		when(venueService.findOne(id)).thenReturn(v);
+		mvc.perform(MockMvcRequestBuilders.get("/venues/updateVenue/{id}", id).with(user("Rob").roles(Security.ADMIN_ROLE))
+				.accept(MediaType.TEXT_HTML))
+		.andExpect(status().isOk()).andExpect(view().name("venues/updateVenue"))
+		.andExpect(handler().methodName("getVenueToUpdate"));
 	}
 
 	@Test
@@ -169,6 +224,134 @@ public class VenuesControllerTest {
 		verify(venueService, never()).save(venue);
 	}
 	
+	@Test
+	public void putVenue() throws Exception {
+		ArgumentCaptor<Venue> arg = ArgumentCaptor.forClass(Venue.class);
+		
+		Venue v = new Venue();
+		v.setName("Venue");
+		v.setCapacity(1000);
+		venueService.save(v);
+		long id = v.getId();
+		when(venueService.findOne(id)).thenReturn(v);
+
+		mvc.perform(MockMvcRequestBuilders.post("/venues/update/{id}", id).with(user("Rob").roles(Security.ADMIN_ROLE))
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("name", "Test Venue New")
+				.param("roadName", "Oxford Rd")
+				.param("postCode", "M13 9GP")
+				.param("capacity", "5000")
+				.accept(MediaType.TEXT_HTML).with(csrf()))
+		.andExpect(status().isFound())
+		.andExpect(view().name("redirect:/venues")).andExpect(model().hasNoErrors())
+		.andExpect(handler().methodName("putVenue"));
+
+		verify(venueService, atLeast(1)).save(arg.capture());
+		verify(venueService).updateLatLonIn(arg.capture());
+	}
+	
+	
+	@Test
+	public void putVenueWithNoName() throws Exception {
+		Venue v = new Venue();
+		v.setName("Venue");
+		v.setCapacity(1000);
+		venueService.save(v);
+		long id = v.getId();
+		when(venueService.findOne(id)).thenReturn(v);
+		mvc.perform(MockMvcRequestBuilders.post("/venues/update/{id}", id).with(user("Rob").roles(Security.ADMIN_ROLE))
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("name", "")
+				.param("roadName", "Oxford Rd")
+				.param("postCode", "M13 9GP")
+				.param("capacity", "5000")
+				.accept(MediaType.TEXT_HTML).with(csrf()))
+		.andExpect(status().isOk())
+		.andExpect(view().name("venues/updateVenue")).andExpect(model().hasNoErrors())
+		.andExpect(handler().methodName("putVenue"));
+
+		verify(venueService, never()).save(venue);
+	}
+	
+	@Test
+	public void putVenueWithNoRoadName() throws Exception {
+		Venue v = new Venue();
+		v.setName("Venue");
+		v.setCapacity(1000);
+		venueService.save(v);
+		long id = v.getId();
+		when(venueService.findOne(id)).thenReturn(v);
+		mvc.perform(MockMvcRequestBuilders.post("/venues/update/{id}", id).with(user("Rob").roles(Security.ADMIN_ROLE))
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("name", "Test")
+				.param("roadName", "")
+				.param("postCode", "M13 9GP")
+				.param("capacity", "5000")
+				.accept(MediaType.TEXT_HTML).with(csrf()))
+		.andExpect(status().isOk())
+		.andExpect(view().name("venues/updateVenue")).andExpect(model().hasNoErrors())
+		.andExpect(handler().methodName("putVenue"));
+
+		verify(venueService, never()).save(venue);
+	}
+	
+
+	@Test
+	@WithMockUser(username = "Mustafa", password = "Mustafa", roles= {"USER"})
+	public void putVenueUnauthorisedUser() throws Exception {
+		when(venueService.findOne(1)).thenReturn(venue);
+		
+		mvc.perform(MockMvcRequestBuilders.post("/venues/update/{id}", 1)
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("name", "Test Venue New")
+				.param("roadName", "Oxford Rd")
+				.param("postCode", "M13 9GP")
+				.param("capacity", "5000")
+				.accept(MediaType.TEXT_HTML).with(csrf()))
+		.andExpect(status().isForbidden());
+	
+		verify(venueService, never()).save(venue);
+	}
+	
+	@Test
+	@WithMockUser(username = "Mustafa", password = "Mustafa", roles= {"ADMINISTRATOR"})
+	public void putVenueNoCsrf() throws Exception {
+		when(venueService.findOne(1)).thenReturn(venue);
+		
+		mvc.perform(MockMvcRequestBuilders.post("/venues/update/{id}", 1)
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("name", "Test Venue New")
+				.param("roadName", "Oxford Rd")
+				.param("postCode", "M13 9GP")
+				.param("capacity", "5000")
+				.accept(MediaType.TEXT_HTML))
+		.andExpect(status().isForbidden());
+
+		
+		verify(venueService, never()).save(venue);
+	}
+	
+
+	/*
+	 * This test should work once form error handling is implemented for update venue
+	@Test
+	public void putEmptyVenue() throws Exception {
+		Venue v = new Venue();
+		v.setName("Venue");
+		v.setCapacity(1000);
+		venueService.save(v);
+		long id = v.getId();
+		when(venueService.findOne(id)).thenReturn(v);
+		mvc.perform(MockMvcRequestBuilders.post("/venues/update/{id}", id).with(user("Rob").roles(Security.ADMIN_ROLE))
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("name", "").accept(MediaType.TEXT_HTML).with(csrf())).andExpect(status().isFound())
+		.andExpect(view().name("venues/updateVenue"))
+		.andExpect(model().attributeHasFieldErrors("venue", "name"))
+		.andExpect(handler().methodName("putVenue"));
+
+		verify(venueService, never()).save(venue);
+	}
+	*/
 	
 	@Test
 	public void getVenue() throws Exception {
@@ -200,6 +383,19 @@ public class VenuesControllerTest {
 		verify(venueService).findOne(id);
 	}
 	
+	@Test
+	public void getVenueWhenVenueDoesNotExist() throws Exception {
+	
+		mvc.perform(MockMvcRequestBuilders.get("/venues/1000").with(user("Rob").roles(Security.ADMIN_ROLE))
+		.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+		.accept(MediaType.TEXT_HTML)
+		.with(csrf()))
+		.andExpect(status().isFound())
+		.andExpect(handler().methodName("showVenueDetails"))
+		.andExpect(view().name("redirect:/venues"));
+		
+		verify(venueService).findOne(1000);
+	}
 
 	@WithMockUser(username = "Mustafa", password = "Mustafa", roles= {"ADMINISTRATOR"})
 	public void deleteVenueByName() throws Exception {
